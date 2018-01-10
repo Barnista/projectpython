bc5b16d504c5c33e9aa55f86487b2b1ce2d3f75e
Barnista <barnista@gmail.com>
2018-01-10 22:01:05 +0700

    Initial commit.

---
+++ my-app/src/main/java/com/mycompany/app/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class HelloWorldTest {
+@Test
+   public void testHelloWorld() {
+assertTrue(true);
+  }
+}
